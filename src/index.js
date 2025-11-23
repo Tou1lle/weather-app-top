ecd98b950fe20cb235eba48cb443cc5fbf8ef4e3
Tuan Ha <hatuan@email.cz>
2025-11-24 00:18:53 +0100

    Get data from VC and create an Object chosen data
    One function is for getting and processing the
    response into json. Then create object accepts the
    Promise created from the json to choose data.
    Promise with fullfilled object is returned.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import { KEY_VISUAL_CROSSING } from "./my-data.js";
 
-console.log(KEY_VISUAL_CROSSING);
+async function getData(location = "Prague") {
+  location = "Berlin"
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&key=${KEY_VISUAL_CROSSING}&contentType=json`
+  );
+  const weatherData = await response.json();
+
+  return weatherData;
+}
+
+async function createWeatherObject(data) {
+  return {
+    address: data.address,
+    temperature: data.currentConditions.temp,
+    description: data.description,
+    time: data.currentConditions.datetime,
+    conditions: data.currentConditions.conditions,
+  };
+}
+
+getData()
+  .then((data) => createWeatherObject(data))
+  .then((dataObj) => console.log(dataObj));
--- src/my-data.js
+++ src/my-data.js
@@ -1,3 +1,4 @@
 const KEY_VISUAL_CROSSING = "9R87HVQRYZLJJLGZHFGMTK5NZ";
+const API_URL_VISUAL_CROSSING = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Prague/today?unitGroup=metric&key=9R87HVQRYZLJJLGZHFGMTK5NZ&contentType=json";
 
-export { KEY_VISUAL_CROSSING };
+export { KEY_VISUAL_CROSSING, API_URL_VISUAL_CROSSING };
