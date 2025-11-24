6628f2cf8123999cdfceb5578314da572a19624a
Tuan Ha <hatuan@email.cz>
2025-11-24 21:57:20 +0100

    Display weather data based on searched place
    The user is able to search a place by writing
    into the input field and then search by pressing
    the search button.
    
    The place shown depends on the searching algorithm
    of Visual Crossing. Also when no valid place is
    returned then a error message is displayed to the
    user (either invalid or empty)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import { KEY_VISUAL_CROSSING } from "./my-data.js";
 
-async function getData(location = "Prague") {
-  location = "Berlin"
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&key=${KEY_VISUAL_CROSSING}&contentType=json`
-  );
-  const weatherData = await response.json();
+const input = document.querySelector("input");
+const searchBtn = document.querySelector("button.search");
+const tempUnitBtn = document.querySelector("button.temp-unit");
 
-  return weatherData;
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-async function createWeatherObject(data) {
+function createWeatherObject(data) {
+  console.log(data);
   return {
-    address: data.address,
+    address: data.resolvedAddress,
     temperature: data.currentConditions.temp,
     description: data.description,
     time: data.currentConditions.datetime,
@@ -20,6 +19,52 @@ async function createWeatherObject(data) {
   };
 }
 
-getData()
-  .then((data) => createWeatherObject(data))
-  .then((dataObj) => console.log(dataObj));
+function createWeatherUI(obj) {
+  const display = document.querySelector(".display-city");
+  display.textContent = "";
+
+  const weatherContainer = document.createElement("div");
+  const city = document.createElement("h2");
+  const temperature = document.createElement("p");
+  const description = document.createElement("p");
+  const conditions = document.createElement("p");
+
+  display.appendChild(weatherContainer);
+  weatherContainer.append(city, temperature, conditions, description);
+
+  city.textContent = capitalize(obj.address);
+  temperature.textContent = obj.temperature;
+  description.textContent = obj.description;
+  conditions.textContent = obj.conditions;
+}
+
+function displayError(error) {
+  const display = document.querySelector(".display-city");
+  display.textContent = error;
+}
+
+async function getData(location = "Prague") {
+  if (!location) {
+    throw new Error("No location added");
+  }
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&key=${KEY_VISUAL_CROSSING}&contentType=json`
+  );
+  if (!response.ok) {
+    throw new Error("Not a valid city");
+  }
+
+  const weatherData = await response.json();
+  return weatherData;
+}
+
+searchBtn.addEventListener("click", () => {
+  const city = input.value;
+  getData(city)
+    .then(createWeatherObject)
+    .then(createWeatherUI)
+    .catch((error) => {
+      console.log(error.message);
+      displayError(error.message);
+    });
+});
